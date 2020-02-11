b321ba1a1d548c920ab0e5ac6e2779db1d11c319
nvelcich <nickvelcich@gmail.com>
2020-02-11 10:47:33 -0600

    Set up Server class.

---
+++ PA1/Server.java
@@ -0,0 +1,11 @@
+import java.io.DataIputStream;
+import java.io.FileInputStream;
+import java.io.IOExeption;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Server extends Thread{
+
+        
+}
