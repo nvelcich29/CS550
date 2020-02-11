a28943d56ca8da4e0bc808f9dd3da158a08a209d
nvelcich <nickvelcich@gmail.com>
2020-02-11 11:54:11 -0600

    typos fixed in the imports

--- PA1/Server.java
+++ PA1/Server.java
@@ -1,6 +1,6 @@
-import java.io.DataIputStream;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.IOExeption;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
